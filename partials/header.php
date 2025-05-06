2556d6532e6266d5f2cceed58ed67f131d566650
waleedamir571 <waleedamir063@gmail.com>
2025-05-06 20:35:11 +0500

    changes

---
+++ partials/header.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/home/favicon-32x32.png">
+    <title>Dentorship</title>
+    <meta name="description"
+        content="Your trusted source for expert healthcare services and medical information. Providing personalized care, advanced treatments, and reliable health resources to help you achieve better health.">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnifying-popup.css">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <!-- Regular -->
+    <!-- <link href="https://db.onlinewebfonts.com/c/xxxxxxxxxx?family=GT+Walsheim+Pro+Regular" rel="stylesheet"> -->
+
+  
+     <!-- <link href="https://db.onlinewebfonts.com/c/yyyyyyyyyy?family=GT+Walsheim+Pro+Medium" rel="stylesheet">
+
+   
+    <link href="https://db.onlinewebfonts.com/c/zzzzzzzzzz?family=GT+Walsheim+Pro+Bold" rel="stylesheet">  -->
+
+</head>
+
+<body>
+
+    <!-- header area start -->
+    <!-- header area start -->
+    <header class="header-one  header--sticky">
+        <div class="header-top-area">
+            <div class="container-full-header">
+                <div class="col-lg-12">
+                    <div class="">
+                       
+                        <div class="">
+                            <p class="text text-center"> Practice your creativity all year long. Save 20% with code CREATE20
+                                &nbsp;&nbsp; <a href="#" class="rts-btn btn-1">Save 20% Today</a></p>
+
+                        </div>
+                        <!-- <div class="right">
+                            <div class="map-area">
+                                <i class="fa-regular fa-phone"></i>
+                                <a href="#">Emergency Line: 1-800-100-900</a>
+                            </div>
+                            <div class="social-area-tranaparent">
+                                <span>Follow Us-</span>
+                                <ul>
+                                    <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                                    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                                </ul>
+                            </div>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-wrapper-1">
+                        <div class="logo-area-start">
+                            <a href="index.php" class="logo">
+                                <img src="assets/images/home/logo.png" alt="logo_area">
+                            </a>
+                            <div class="nav-area">
+                                <ul class="">
+
+                                    <li class="main-nav"><a href="#">Browse</a></li>
+                                    <div class="header-right">
+                                        <div class="input-area">
+                                            <input id="myInput" type="text" placeholder="Search...">
+                                            <i class="fa-light fa-magnifying-glass"></i>
+                                        </div>
+                                        <li class="main-nav"><a href="#"><img src="assets/images/home/country.png"
+                                                    alt=""></a></li>
+                                        <li class="main-nav"><a href="#">Book</a></li>
+                                        <li class="main-nav"><a href="#">Log in</a></li>
+                                        <!-- <div class="cart-button">
+                                        <i class="fa-light fa-cart-shopping"></i>
+                                    </div> -->
+                                        <a href="#" class="rts-btn btn-2">Sign Up </a>
+                                        <div class="menu-btn" id="menu-btn">
+                                            <svg width="20" height="16" viewBox="0 0 20 16" fill="none"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <rect y="14" width="20" height="2" fill="#1F1F25"></rect>
+                                                <rect y="7" width="20" height="2" fill="#1F1F25"></rect>
+                                                <rect width="20" height="2" fill="#1F1F25"></rect>
+                                            </svg>
+                                        </div>
+                                    </div>
+
+                                </ul>
+
+
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
